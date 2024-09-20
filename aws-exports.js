23d1577f9f07ac752f1b52dba6ac0bcfcc772152
1Roman1126 <133667323+1Roman1126@users.noreply.github.com>
2024-09-20 12:54:56 +0545

    Update aws-exports.js

--- aws-exports.js
+++ aws-exports.js
@@ -2,6 +2,8 @@ const awsmobile = {
     "aws_project_region": "us-east-1",
     "aws_user_files_s3_bucket": "Nepse-stock-data",  // Your S3 bucket name
     "aws_user_files_s3_bucket_region": "us-east-1",  // The region where your S3 bucket is located
+    "aws_user_pools_id": "us-east-1_MjcuAeEtX",
+     "aws_user_pools_web_client_id": "676a9kep4q26qsotlu86hpqsfn",
 };
 
 export default awsmobile;
