88a25354e5d2cbad1d571e7568e1680d5dec392f
1Roman1126 <133667323+1Roman1126@users.noreply.github.com>
2024-09-21 07:49:09 +0545

    Update aws-exports.js

--- aws-exports.js
+++ aws-exports.js
@@ -1,9 +1,10 @@
-const awsmobile = {
-    "aws_project_region": "us-east-1",
-    "aws_user_files_s3_bucket": "Nepse-stock-data",  // Your S3 bucket name
-    "aws_user_files_s3_bucket_region": "us-east-1",  // The region where your S3 bucket is located
-    "aws_user_pools_id": "us-east-1_MjcuAeEtX",
-     "aws_user_pools_web_client_id": "676a9kep4q26qsotlu86hpqsfn",
-};
+import Amplify, { Storage } from 'aws-amplify';
 
-export default awsmobile;
+Amplify.configure({
+    Storage: {
+        AWSS3: {
+            bucket: 'nepse-stock-data', // Replace with your S3 bucket name
+            region: 'us-east-1', // Replace with your region
+               }
+    }
+});
